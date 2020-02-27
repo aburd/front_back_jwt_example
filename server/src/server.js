4879b1db1a97236d66b11bb400aa85b8c1f26543
aburd <burdick.aaron@gmail.com>
2020-02-28 01:28:24 +0900

    add server logging

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 const userRoutes = require('./routes/users')
 
+// Logging
+app.use(morgan('combined'))
+
+// Routing
 app.use('/api/users', userRoutes)
 
 const port = process.env.PORT || 3000
